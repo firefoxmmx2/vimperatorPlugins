e1030e3f76b8fa6a52d95378be9b095297006eb2
firefoxmmx2 <firefoxmmx@gmail.com>
2012-09-19 00:41:52 +0800

    去除对jquery依赖，解决xmlhttprequest2的问题。未完待续

--- ehentai-downloader.js
+++ ehentai-downloader.js
@@ -1,21 +1,9 @@
 // ehentai downloader.js
-// 下载 ehentai 上面的漫画。此插件依赖于JQUERY 和 jquery-loader.js插件
+// 下载 ehentai 上面的漫画。
 
 (function(){
-    //简化函数
-    var $ = jQuery.core;
     let self = liberator.plugins.ehentai = (function(){
         //注册命令 
-        commands.addUserCommand(
-            ['ehmanager','ehm'],
-            'ehentai漫画下载宏命令',
-            function(arg){ //该命令应该支持子命令。目前还未实现。
-                self.currTabDownload();
-            },
-            null,
-            true
-            );
-
 
         //返回操作对象
         var ehentaiManager = {
@@ -26,28 +14,52 @@
                 var window = content.window.wrappedJSObject;
                 var title;
                 //默认间隔时间5秒
-                var intervalTime = 4000;
-
+                var intervalTime = 1000;
+                var lateTime = 500;
+                var amplification = 200;
                 //窗口的详情页面判断
-                if(/^http:\/\/g\.e-hentai.org\/g\/([\w-]+)/([\w-]+)\/$/.exec(window.location.href)){
+                if(/^http:\/\/g\.e-hentai\.org\/g\/([\w-]+)\/([\w-]+)\/$/.exec(window.location.href)){
                     //如果有副标题使用副标题，作为保存文件夹
-                    title = $('#gd2 h1:last',doc).text();
-
+                    title = doc.querySelector('#gd2 h1:last-child').innerText;
+                    
                     var downPicture = function(){
-                        $('#gdt a').each(function(idx){
-                            var a = $(this);
-                            $('<div></div>').attr('name',a.attr('href'))
-                                .appendTo(doc).hide()
-                                .load(a.attr('href'),null,function(data){
-                                       console.log($('.sin>a>img').attr('src'));
+                        var as = doc.querySelectorAll('#gdt a');
+                       
+                        function goDetailPage(a) {
+                            var xhr = new XMLHttpRequest();
+                            xhr.open('GET', a.getAttribute('href'), true);
+                            xhr.responseType = 'document';
+                            xhr.onload = function(e){
+                                if(this.status == 200){
+                                    var img = this.response.body.querySelector('.sin > a > img');
+                                    alert(img.getAttribute('src'));
+                                    //timer.clearInterval();
                                 }
-                                );
-                        });
+                            };
+
+                            xhr.send();
+
+                        }
+
+                        for(var i=0;i<as.length;i++){
+                            var a = as[i];
+
+                            setTimeout(function(){goDetailPage(a);},lateTime);
+                            lateTime += amplification;
+
+                        }
+
                     };
-                    var timer = setInterval(downPicture,intervalTime);
+
+                    setTimeout(downPicture,lateTime);
+                   /* var timer = setInterval(*/
+                            //function(){
+                                //setTimeout(downPicture,lateTime);
+                            /*},intervalTime);*/
                 }
-                
+
             }
+
         };
 
         return ehentaiManager;
