f861726c68451ba28bdfbe3f708c822c2b65a941
firefoxmmx2 <firefoxmmx@gmail.com>
2012-10-05 20:48:00 +0800

    移出jquery依赖,改为html5方式的

--- open-hentai-megahit.js
+++ open-hentai-megahit.js
@@ -14,26 +14,54 @@
             );
         //}}}
 
+        // private 
+        // megahit month page template 
+        // para {date} is target date, format as yyMM
+        var megahitMonthPageTemplate = "http://www.megahit.co.jp/comic/comiclist{date}.htm";
+        //a format date function
+        var formatDate = function(needFormatDate){
+            var formatedDate;
+
+            return formatedDate;
+        }
         //return one
         var PUBLICS={
             open:function(arg){
-                if(jQuery.core('table b:contains("成年コミック　発売予定表")',content.document.body).length){
-                    var searchpage="http://sukebei.nyaa.eu/?page=torrents&page=search&cats=0_0&filter=0&term={keyword}";
-                    var $container=jQuery.core('table',content.document.body).eq(1);
-                    var maxTdNum=$container.find('tr:first td').length;
-                    $container.find('tr')
-                        .each(function(idx){
-                            var $tr = jQuery.core(this);
-                            var text;
-                            if($tr.find('td').length<maxTdNum)
-                                text=$tr.find('td:nth(1)').text().replace('　',' ');
-                            else
-                                text=$tr.find('td:nth(2)').text().replace('　',' ');
-                            var url=searchpage.replace('\{keyword\}',text);
-                            window.openNewTabWith(url);
+                var megahitPage = megahitMonthPageTemplate.replace('\{date\}',arg,'g');
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET',megahitPage,true);
+                xhr.responseType = 'document';
+                xhr.onload = function(e){
+                    if(this.status == 200){
+                        var doc = this.response.body;
+                        var result = false;
+                        Array.prototype.forEach.call(doc.querySelectorAll('table b'),function(b){
+                            if(b.textContent == "成年コミック　発売予定表"){
+                                result = true;
+                            }
                         });
-                }
 
+                        if(result){
+                            var searchpage="http://sukebei.nyaa.eu/?page=torrents&page=search&cats=0_0&filter=0&term={keyword}";
+                            var $container=doc.querySelectorAll('table')[1];
+                            var maxTdNum=$container.querySelectorAll('tr:first-child td').length;
+
+                            Array.prototype.forEach.call($container.querySelectorAll('tr'),function(tr){
+                                var text;
+
+                                if(tr.querySelectorAll('td').length < maxTdNum)
+                                    text=tr.querySelector('td:nth-child(2)').textContent.replace('　|・',' ');
+                                else
+                                    text=tr.querySelector('td:nth-child(3)').textContent.replace('　|・',' ');
+                                var url=searchpage.replace('\{keyword\}',text);
+                                window.openNewTabWith(url);
+                            });
+                        }
+
+
+                    }
+                };
+                xhr.send();
             }
         };
 
